96b01062da0525de1d1f4a291c6409c208f39d2a
Roman Ponomarev <roman.ponomarev66@gmail.com>
2022-01-05 20:54:38 +0300

    feat: add adaptive js, fix: js pagination

--- our-pets.js
+++ our-pets.js
@@ -110,7 +110,67 @@ const createChunks = (items, size) => {
     return chunks
 }
 
-const chunks = createChunks(arr, 8)
+let cardsPerPage = 8
+
+if (window.screen.width <= 320) {
+    cardsPerPage = 3
+    let orgHtml = document.querySelector('.menu')
+    const oldParent = orgHtml.parentNode;
+    let burger = document.createElement('div')
+    burger.classList.add('menu__burger')
+    oldParent.replaceChild(burger, orgHtml)
+    burger.appendChild(orgHtml)
+    const burgerBtn = document.createElement('button')
+    burgerBtn.classList.add('burgerBtn')
+    burgerBtn.innerHTML = `<img src="img/Burger-ourpets.png" class = "burger__btn-img">`;
+    orgHtml.before(burgerBtn)
+}
+
+const btn = document.querySelector('.burgerBtn')
+if (btn) {
+    const burger = document.querySelector('.menu')
+    btn.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock')
+        btn.classList.toggle('_active')
+        burger.classList.toggle('_active')
+    })
+}
+
+const chunks = createChunks(arr, cardsPerPage)
+
+const renderCards = (cardList) => {
+    const cardsWrapper = document.querySelector('.our__pets-cards')
+    cardsWrapper.innerHTML = ''
+   
+    for (let index = 0; index < cardList.length; index++) {
+        const element = cardList[index];
+
+        const card  = document.createElement('div')
+        card.className = 'slider__item'
+
+        const cardImg = document.createElement('img')
+        cardImg.src = element.src
+        cardImg.alt = ''
+
+        const cardName = document.createElement('p')
+        cardName.className = 'card__name'
+        cardName.innerHTML = element.name
+
+        const cardBtn = document.createElement('button')
+        cardBtn.className = 'card__btn'
+        cardBtn.innerHTML = 'Learn more'
+
+        card.appendChild(cardImg)
+        card.appendChild(cardName)
+        card.appendChild(cardBtn)
+
+        cardsWrapper.appendChild(card)
+    }
+}
+const prevBtn = document.querySelector('.btn__prev')
+const startBtn = document.querySelector('.btn__start')
+const nextBtn = document.querySelector('.btn__next')
+const endBtn = document.querySelector('.btn__end')
 
 const showPage = (action) => {
     const currentPageNode = document.querySelector('.btn__number')
@@ -126,20 +186,35 @@ const showPage = (action) => {
         newPage = chunks.length
     }
 
+    console.log(newPage)
+
+    if (newPage === 1) {
+        prevBtn.classList.add('btn-disabled')
+        startBtn.classList.add('btn-disabled')
+    } else {
+        prevBtn.classList.remove('btn-disabled')
+        startBtn.classList.remove('btn-disabled')
+    }
+
+    if (newPage === chunks.length) {
+        nextBtn.classList.add('btn-disabled')
+        endBtn.classList.add('btn-disabled')
+    } else {
+        nextBtn.classList.remove('btn-disabled')
+        endBtn.classList.remove('btn-disabled')
+    }
+    
+
     currentPageNode.innerHTML = newPage
-    console.log(chunks[newPage - 1])
+    renderCards(chunks[newPage - 1])
 }
 
 showPage()
 
-const prevBtn = document.querySelector('.btn__prev')
+
 prevBtn.addEventListener('click', () => { showPage('prev') })
-
-const nextBtn = document.querySelector('.btn__next')
-nextBtn.addEventListener('click', () => { showPage('next') })
-
-const startBtn = document.querySelector('.btn__start')
 startBtn.addEventListener('click', () => { showPage() })
 
-const endBtn = document.querySelector('.btn__end')
+
+nextBtn.addEventListener('click', () => { showPage('next') })
 endBtn.addEventListener('click', () => { showPage('end') })
